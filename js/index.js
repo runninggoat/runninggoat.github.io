efdb03082cf54dccd8a4bcae58d7702721c8f690
runninggoat <1501832474@qq.com>
2018-12-09 12:48:53 +0800

    博客雏形以及第一篇博文

--- js/index.js
+++ js/index.js
@@ -1,30 +1,139 @@
 const Component = React.Component
 const {
+  Grid,
   Button,
-  colors,
-  createMuiTheme,
   CssBaseline,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Icon,
-  MuiThemeProvider,
   Typography,
-  withStyles,
-  classes,
+  Paper,
 } = window['material-ui']
+const tags = ['ThreeJS', 'ReactJS', '其它', 'ReactJS1', '其它1', 'ReactJS2', '其它2']
 
 class Index extends Component {
+  initTags () {
+    return tags.map((tag, idx) => {
+      return (
+        <Grid item key={tag} style={{ padding: '10px' }}>
+          <Typography
+            variant='h6'
+            align='center'
+          >{tag}</Typography>
+        </Grid>
+      )
+    })
+  }
+
+  initBlogs () {
+    function initParagraphs (paragraphs) {
+      return paragraphs.map((paragraph, idx) => {
+        let element = null
+        switch (paragraph.type) {
+          case 'text':
+            element = (
+              <Typography
+                key={idx}
+                variant='body2'
+                align='left'
+                style={{ margin: '5px 0' }}
+              >{ paragraph.content }</Typography>
+            )
+            break;
+          case 'image':
+            element = (
+              <div
+                key={idx}
+                style={{ textAlign: 'center' }}
+              >
+                <img src={paragraph.content} />
+              </div>
+            )
+          default:
+            break;
+        }
+        return element
+      })
+    }
+    return blogs.map((blog, idx) => {
+      let content = initParagraphs(blog.paragraphs)
+      return (
+        <Grid
+          key={ blog.uid }
+          container
+          justify='center'
+          style={{ margin: '10px 0' }}
+        >
+          <Grid
+            item
+            sm={6}
+          >
+            <Paper style={{
+              padding: '10px 30px',
+            }}>
+              <Grid
+                container
+                justify='center'
+              >
+                <Grid item sm={12}>
+                  <Typography
+                    variant='h6'
+                    align='left'
+                  >{ blog.title }</Typography>
+                  { content }
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      )
+    })
+  }
+
   render () {
+    let tagsGrids = this.initTags()
+    let blogsGrids = this.initBlogs()
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <Button
-          variant='contained'
-          color='default'
-        >Click me!</Button>
+        <CssBaseline />
+        <Grid
+          container
+          justify='center'
+        >
+          <Grid
+            item
+            sm={10}
+            style={{ padding: '20px', borderBottom: '1px solid #545454' }}
+          >
+            <Typography
+              variant='h4'
+              align='center'
+            >以将图南</Typography>
+          </Grid>
+          <Grid
+            item
+            sm={10}
+          >
+            <Grid
+              container
+              justify='space-around'
+            >
+              { tagsGrids }
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          justify='center'
+          style={{ margin: '20px 0' }}
+        >
+          <Grid
+            item
+            sm={10}
+          >
+            <Paper
+              style={{ height: '300px', backgroundColor: '#000' }}
+            ></Paper>
+          </Grid>
+        </Grid>
+        { blogsGrids }
       </div>
     )
   }
